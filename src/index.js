90f1670cd7047133659ccd8c06255daebb204bf6
Umer Khalil <23108194@szabist-isb.pk>
2026-08-10 09:49:30 +0500

    Update webVersionCache to 2.3000.1014111620-alpha for faster WhatsApp Web loading

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ const client = new Client({
   },
   webVersionCache: {
     type: 'remote',
-    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1014111620-alpha.html',
   }
 });
 
